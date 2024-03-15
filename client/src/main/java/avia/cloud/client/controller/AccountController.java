dde96adc678be86c88db1485283e4fb56b24313b
Kyialbek-Temirbekov <28112002akn@gmail.com>
2024-03-15 11:26:23 +0600

    added email check api

--- client/src/main/java/avia/cloud/client/controller/AccountController.java
+++ client/src/main/java/avia/cloud/client/controller/AccountController.java
@@ -7,6 +7,7 @@ import avia.cloud.client.dto.VerificationInfo;
 import avia.cloud.client.entity.enums.Role;
 import avia.cloud.client.service.IAccountService;
 import avia.cloud.client.service.IAuthorityService;
+import avia.cloud.client.validation.constraint.NotRegistered;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,11 @@ public class AccountController {
     @GetMapping("/refresh")
     public ResponseEntity<ResponseDTO> refresh() {
         return ResponseEntity.ok(new ResponseDTO("200","Access token received successfully"));
+    }
+
+    @GetMapping("/checkEmail")
+    public ResponseEntity<?> checkEmail(@RequestParam @NotRegistered String email) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
     @DeleteMapping("/removeAll")
     public ResponseEntity<?> removeAll() {
--- client/src/main/java/avia/cloud/client/dto/AddressDTO.java
+++ client/src/main/java/avia/cloud/client/dto/AddressDTO.java
@@ -3,7 +3,13 @@ package avia.cloud.client.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressDTO {
     @NotNull
     @NotBlank
